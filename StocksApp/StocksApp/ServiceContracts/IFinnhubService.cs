04af295124af742f654a8da288da70910e247805
Givi <109596887+Givikoo01@users.noreply.github.com>
2024-01-20 01:05:53 +0400

    Delete StocksApp directory

--- StocksApp/StocksApp/ServiceContracts/IFinnhubService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace StocksApp.ServiceContracts
-{
-	public interface IFinnhubService
-	{
-		public Dictionary<string, object>? GetCompanyProfile(string stockSymbol);
-		public Dictionary<string, object>? GetStockPriceQuote(string stockSymbol);
-	}
-}
